5729fc2f92c15d4c754033749777b5d3681bca87
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-05-30 21:43:38 +0900

    Update st.markdown().py

--- st.markdown().py
+++ st.markdown().py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.markdown('Streamlit은 **_매우_ 훌륭하다**.')
-st.markdown("이 문장은 :red[colored red], 그리고 이문장은 **:blue[colored]** 그리고 볼드체이다.")
+st.markdown("이 문장은 :red[빨강], 그리고 이문장은 **:blue[파랑]** 그리고 볼드체이다.")
 st.markdown(":green[$\sqrt{x^2+y^2}=1$] 피타고라스 항등식이다. :pencil:")
