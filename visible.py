dd04519a18255a9222602d69821bcb1b29237ff0
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-12 22:50:45 +0900

    Update visible.py

--- visible.py
+++ visible.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-uploaded_file = st.file_uploader("파일을 선택해주세요", type = ['png', 'jpg'], label_visibility = "hidden")
+uploaded_file = st.file_uploader("파일을 선택해주세요", type = ['png', 'jpg'])
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("파일 이름:", uploaded_file.name)
