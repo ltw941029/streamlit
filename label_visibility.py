c147dbd7a77f46581762856d2b12995ceb6254ed
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-12 23:05:28 +0900

    Update label_visibility.py

--- label_visibility.py
+++ label_visibility.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 uploaded_file = st.file_uploader("파일을 선택해주세요", key=1, label_visibility='collapsed')
 
-uploaded_file = st.file_uploader("파일을 선택해주세요", key=2, label_visibility='visibl')
+uploaded_file = st.file_uploader("파일을 선택해주세요", key=2, label_visibility='visible')
 
 uploaded_file = st.file_uploader("파일을 선택해주세요", key=3, label_visibility='hidden')
