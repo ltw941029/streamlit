79d1806dfa53ef43a82baf4b0252faf7ae42aa90
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-12 22:36:41 +0900

    Create use_container_width.py

---
+++ use_container_width.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame(
+    [
+       {"명령어": "st.selectbox", "평점": 4, "is_widget": True},
+       {"명령어": "st.balloons", "평점": 5, "is_widget": False},
+       {"명령어": "st.time_input", "평점": 3, "is_widget": True},
+   ]
+)
+
+edited_df = st.experimental_data_editor(df, width = 200, use_container_width = True, num_rows="fixed")
+
+favorite_command = edited_df.loc[edited_df["평점"].idxmax()]["명령어"]
+
+edited_df = st.experimental_data_editor(df, width = 200, use_container_width = False, num_rows="fixed")
+
+favorite_command = edited_df.loc[edited_df["평점"].idxmax()]["명령어"]
