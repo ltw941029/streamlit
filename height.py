c7a53f873652778482f6df61b0adbf56b73a44e7
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-12 22:35:35 +0900

    Update height.py

--- height.py
+++ height.py
@@ -8,7 +8,7 @@ df = pd.DataFrame(
        {"명령어": "st.time_input", "평점": 3, "is_widget": True},
    ]
 )
-edited_df = st.experimental_data_editor(df, height = 2000, num_rows="fixed")
+edited_df = st.experimental_data_editor(df, height = 500, num_rows="fixed")
 
 favorite_command = edited_df.loc[edited_df["평점"].idxmax()]["명령어"]
 
